7ceb3cdabb923c8732da52a8e66591681fb0ffa7
Viny-Pereira <vinypereira75@gmail.com>
2023-07-21 21:31:29 -0300

    correcoes

--- AULAS/Aula_5/src/LadosInvalidosException.java
+++ AULAS/Aula_5/src/LadosInvalidosException.java
@@ -1,9 +1,6 @@
 package Aula_5.src;
 
 public class LadosInvalidosException extends Exception{
-    public LadosInvalidosException(){
-        super();
-    }
     public LadosInvalidosException(String msg){
         super(msg);
     }
---
+++ AULAS/Aula_5/src/QuantidadeInvalidasException.java
@@ -0,0 +1,7 @@
+package Aula_5.src;
+
+public class QuantidadeInvalidasException extends RuntimeException {
+    public QuantidadeInvalidasException(String msg) {
+        super(msg);
+    }
+}
