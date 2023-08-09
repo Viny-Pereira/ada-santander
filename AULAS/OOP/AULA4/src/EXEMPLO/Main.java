881e33ce5cd147a5cae844e9ba7e880af53a72eb
Viny-Pereira <vinypereira75@gmail.com>
2023-08-09 20:00:29 -0300

    Aula 4 - Exemplo de encapsulamento

---
+++ AULAS/OOP/AULA4/src/EXEMPLO/Main.java
@@ -0,0 +1,9 @@
+package OOP.AULA4.src.EXEMPLO;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "viny";
+        int idade = 18;
+        new Pessoa(nome, idade);
+    }
+}
---
+++ AULAS/OOP/AULA4/src/EXEMPLO/PessoaFisica.java
@@ -0,0 +1,10 @@
+package OOP.AULA4.src.EXEMPLO;
+
+public class PessoaFisica extends Pessoa{
+
+    public PessoaFisica(String nome, int idade){
+        super(nome, idade);
+    }
+
+
+}
