0f9f93df0ceb7e6eebdc77d60aee917459150a8c
Viny-Pereira <vinypereira75@gmail.com>
2023-08-10 21:48:17 -0300

    Aula 4 - Aplicacao cricacao da classe restaurante

---
+++ AULAS/OOP/AULA4/src/EXERCICIO/domain/Restaurante.java
@@ -0,0 +1,48 @@
+package OOP.AULA4.src.EXERCICIO.domain;
+
+import java.util.List;
+
+public class Restaurante {
+    private String nomeRestaurante;
+    private String endereco;
+    private List<Pratos> pratos;
+
+    public Restaurante(String nomeRestaurante, String endereco, List<Pratos> pratos) {
+        this.nomeRestaurante = nomeRestaurante;
+        this.endereco = endereco;
+        this.pratos = pratos;
+    }
+
+    public String getNomeRestaurante() {
+        return nomeRestaurante;
+    }
+
+    public void setNomeRestaurante(String nomeRestaurante) {
+        this.nomeRestaurante = nomeRestaurante;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Pratos> getPratos() {
+        return pratos;
+    }
+
+    public void setPratos(List<Pratos> pratos) {
+        this.pratos = pratos;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurante{" +
+                "nomeRestaurante='" + nomeRestaurante + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", pratos=" + pratos +
+                '}';
+    }
+}
