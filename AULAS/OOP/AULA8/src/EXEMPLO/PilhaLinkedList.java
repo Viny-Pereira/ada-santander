f2240598b55d5ebf9a4ee4e73870c08f2b72673d
Viny-Pereira <vinypereira75@gmail.com>
2023-08-18 20:20:54 -0300

    Aula 8 - Pilha Linked List

--- AULAS/OOP/AULA8/src/EXEMPLO/PilhaLinkedList.java
+++ AULAS/OOP/AULA8/src/EXEMPLO/PilhaLinkedList.java
@@ -1,4 +1,25 @@
 package OOP.AULA8.src.EXEMPLO;
 
-public class PilhaLinkedList extends Pilha{
+import java.util.LinkedList;
+
+public class PilhaLinkedList extends Pilha {
+    private final LinkedList<Object> lista = new LinkedList<>();
+
+    @Override
+    public void push(Object obj) {
+        lista.addLast(obj);
+
+    }
+
+    @Override
+    public Object pop() {
+        var temp = peek();
+        lista.removeLast();
+        return temp;
+    }
+
+    @Override
+    public Object peek() {
+        return lista.getLast();
+    }
 }
