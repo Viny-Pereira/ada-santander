e77f118806b93e72aa3b723330f24daf411292c8
Viny-Pereira <vinypereira75@gmail.com>
2023-08-18 20:16:54 -0300

    Aula 8 - Pilha test

--- AULAS/OOP/AULA8/src/EXEMPLO/Pilha.java
+++ AULAS/OOP/AULA8/src/EXEMPLO/Pilha.java
@@ -1,4 +1,11 @@
 package OOP.AULA8.src.EXEMPLO;
 
 public abstract class Pilha {
+
+    public abstract void push(Object obj);
+
+    public abstract Object pop();
+
+    public abstract Object peek();
+
 }
