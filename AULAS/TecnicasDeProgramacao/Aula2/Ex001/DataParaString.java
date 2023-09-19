252cfef087463fddf4f8ca02a89adef207b1ecf2
Viny-Pereira <vinypereira75@gmail.com>
2023-09-18 22:22:08 -0300

    Aula 2 - Trabalhando com datas

--- AULAS/TecnicasDeProgramacao/Aula2/Ex001/DataParaString.java
+++ AULAS/TecnicasDeProgramacao/Aula2/Ex001/DataParaString.java
@@ -3,6 +3,7 @@ package TecnicasDeProgramacao.Aula2.Ex001;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DataParaString {
     public static void main(String[] args) {
@@ -11,8 +12,13 @@ public class DataParaString {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        DateTimeFormatter dateTimeEstenso = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyyy");
+        DateTimeFormatter dateTimeEstensoIngles = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyyy", Locale.US);
+
 
         System.out.println(hoje.format(formatter));
         System.out.println(agora.format(dateTimeFormatter));
+        System.out.println(hoje.format(dateTimeEstenso));
+        System.out.println(hoje.format(dateTimeEstensoIngles));
     }
 }
--- AULAS/TecnicasDeProgramacao/Aula2/Ex001/FusoHorario.java
+++ AULAS/TecnicasDeProgramacao/Aula2/Ex001/FusoHorario.java
@@ -1,2 +1,27 @@
-package TecnicasDeProgramacao.Aula2.Ex001;public class FusoHorario {
+package TecnicasDeProgramacao.Aula2.Ex001;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+public class FusoHorario {
+    public static void main(String[] args) {
+        /*
+        for (String availableZoneId : ZoneId.getAvailableZoneIds()) {
+            System.out.println(availableZoneId);
+        }*/
+
+        System.out.println(ZoneId.getAvailableZoneIds().size());
+
+        LocalDateTime dataHoraAtual = LocalDateTime.now();
+        System.out.println(dataHoraAtual);
+
+        ZonedDateTime dataHoraAtualComFusoHario = ZonedDateTime.now();
+        System.out.println(dataHoraAtualComFusoHario);
+        ZonedDateTime dataHoraParis = dataHoraAtualComFusoHario.withZoneSameInstant(ZoneId.of("Europe/Paris"));
+        System.out.println(dataHoraParis);
+
+    }
 }
